bcaa4d5af154469d0f17469ec62990c1569622c7
ivanreveco <129100347+ivanreveco@users.noreply.github.com>
2023-12-11 22:44:23 -0300

    POO

---
+++ ejercicios/POO/Persona.py
@@ -0,0 +1,7 @@
+class Persona:
+    def __init__(self):#inicializar
+        self.nombre = 'juan'#agregar atributos
+        self.apellido = 'enriquez'
+        self.edad = 19
+persona1 = Persona()
+print(persona1.edad)#para aceder a un atributo
