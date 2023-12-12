5070ffa1239d65ea2dd75ba17d0e07aca6acaeae
ivanreveco <129100347+ivanreveco@users.noreply.github.com>
2023-12-11 22:35:33 -0300

    a

---
+++ ejercicios/funciones recursivas.py
@@ -0,0 +1,17 @@
+#def factorial(numero):
+#    if numero==1:
+#       return 1
+#    else:
+#        return numero*factorial(numero-1)
+#resultado = factorial(5)
+#print(f"el factorial de 100 es : {resultado}")
+#=================================================
+#def decender(numero):#descender en numero en funcion recursiva
+#    if numero >=1:
+#        print(numero)
+#        decender(numero-1)
+#    else:
+#        return 0
+#decender(5)
+#==================================================
+  
