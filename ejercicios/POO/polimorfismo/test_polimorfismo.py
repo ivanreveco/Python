ffe97e099daba1d4609f5481a6511d889774df6a
ivanreveco <129100347+ivanreveco@users.noreply.github.com>
2023-12-26 18:21:34 -0300

    a

--- ejercicios/POO/polimorfismo/test_polimorfismo.py
+++ ejercicios/POO/polimorfismo/test_polimorfismo.py
@@ -5,6 +5,9 @@ def imprmir_detalles(objeto):
     print(objeto)
     print(type(objeto))
     
+    if isinstance(objeto, Gerente):# hace referencia a que si es una instancia de un objeto en este caso gerente que te imprima el departamento de lo contrario no lo hara
+        print(objeto.departamento)
+    
 empleado = Empleado('juan',5000)
 imprmir_detalles(empleado)
 
